39ead69665a8fd597bf1e8e6fe4c66c69815f38b
Carolyne Lira <carolyne.lira@meliuz.com.br>
2022-04-03 19:00:53 -0300

    finish

--- hangman_game.py
+++ hangman_game.py
@@ -1,12 +1,12 @@
-def play_hangman():
-    print();
-    print("***************************");
-    print("Welcome to the hangman game");
-    print("***************************");
-    print();
+import random;
 
-    secret_word = "chocolate";
-    found_letters = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+def play_hangman():
+
+    opening_message();
+
+    secret_word = load_secret_word();
+
+    found_letters = initialize_correct_words(secret_word);
 
     hanged = False;
     won = False;
@@ -16,27 +16,119 @@ def play_hangman():
     print();
 
     while (not hanged and not won):
-        # print('hanged', hanged)
-        guess = input("Type a letter: ");
-        guessFormatted = guess.strip().lower();
+        guessFormatted = ask_for_a_letter();
 
         if (guessFormatted in secret_word):
-            index = 0;
-
-            for letter in secret_word:
-                if (guessFormatted == letter):
-                    found_letters[index] = letter
-                index = index + 1;
+            save_correct_letters(secret_word, guessFormatted, found_letters);
         else:
-            attempts = attempts + 1;
-            # print('attempts', attempts)
+            attempts += 1;
+            draw_hangman(attempts);
 
-        hanged = attempts == 6;
+        hanged = attempts == 7;
+        won = "_" not in found_letters;
         print(found_letters);
 
-        print("jogando...");
+    if (won):
+        won_message();
+    else:
+        lose_message(secret_word);
 
     print("Fim do jogo");
+
+def opening_message():
+    print();
+    print("***************************");
+    print("Welcome to the hangman game");
+    print("***************************");
+    print();
+
+def load_secret_word():
+    file = open("words.txt", "r");
+    words_list = [];
+
+    for line in file:
+        line = line.strip();
+        words_list.append(line);
+
+    file.close();
+
+    word_index = random.randrange(0, len(words_list));
+    secret_word = words_list[word_index].upper();
+
+    return secret_word;
+
+def initialize_correct_words(secret_word):
+    return ["_" for letter in secret_word];
+
+def ask_for_a_letter():
+    guess = input("Type a letter: ");
+    return guess.strip().upper();
+
+def save_correct_letters(secret_word, guessFormatted, found_letters):
+    index = 0;
+
+    for letter in secret_word:
+        if (guessFormatted == letter):
+            found_letters[index] = letter;
+        index += 1;
+
+def won_message():
+    print("Você acertou!");
+
+def lose_message(secret_word):
+    print("Você perdeu :(");
+    print();
+    print("A palavra secreta era: {}".format(secret_word));
+
+def draw_hangman(attempts):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(attempts == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(attempts == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(attempts == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(attempts == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(attempts == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(attempts == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (attempts == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 if (__name__ == "__main__"):
     play_hangman();
